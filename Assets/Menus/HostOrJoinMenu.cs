83ddc29e6f025e17e23062ed1cac71dccbe20496
titou4n <titou4n.simon@gmail.com>
2025-01-16 09:09:25 +0100

    commit for push

--- Assets/Menus/HostOrJoinMenu.cs
+++ Assets/Menus/HostOrJoinMenu.cs
@@ -1,15 +1,38 @@
+using Steamworks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class HostOrJoinMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        if (!SteamManager.Initialized)
+        {
+            Debug.LogError("Steam is not initialized.");
+            return;
+        }
+    }
+
+    public void OpenFriendsOverlay()
+    {
+        if (SteamManager.Initialized)
+        {
+            //SteamFriends.ActivateGameOverlay("Friends");
+            SceneManager.LoadScene("Scenes/BuildingsScene");
+        }
+        else
+        {
+            Debug.LogError("Steam is not initialized or not running.");
+        }
+    }
+    
     public void Host()
     {
-        Debug.Log("Option1");
+        OpenFriendsOverlay();
     }
     
     public void Join()
     {
-        Debug.Log("Option2");
+        OpenFriendsOverlay();
     }
 }

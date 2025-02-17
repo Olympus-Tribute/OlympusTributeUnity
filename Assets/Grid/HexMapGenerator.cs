6477f3a4cd483390ba081179cc3fcfa5280d970a
Gaël Gentric <gael.gentric@epita.fr>
2025-02-17 16:35:31 +0100

    2nd big refactor

--- Assets/Grid/HexMapGenerator.cs
+++ Assets/Grid/HexMapGenerator.cs
@@ -1,13 +1,13 @@
 using System;
 using OlympusWorldGenerator;
-using OlympusWorldGenerator.Generators;
 using UnityEngine;
+using Object = UnityEngine.Object;
 using Random = System.Random;
 
 
 namespace Grid
 {
-    public class HexMapGenerator : MonoBehaviour, IFloorGrid
+    public class HexMapGenerator : IFloorGrid
     {
         private const float hexSize = 10f; // longueur d'un coté hexagone
         private const double sqrt3 = 1.7320508076d;
@@ -15,14 +15,14 @@ namespace Grid
         private readonly int mapHeight = 10; // Nombre d'hexagones en hauteur
     
         private readonly GameObject grassPrefabs;
-        private readonly GameObject waterPrefabs;
+        private readonly GameObject oceanPrefabs;
         private readonly GameObject woodresourcePrefabs;
         private readonly GameObject diamondresourcePrefabs;
         private readonly GameObject obsidianresourcePrefabs;
         private readonly GameObject goldresourcePrefabs;
         private readonly GameObject stoneresourcePrefabs;
         private readonly GameObject vineresourcePrefabs;
-        private readonly GameObject waterresourcePrefabs;
+        private readonly GameObject lakePrefabs;
         
     
         private readonly FloorTile[] Tiles;
@@ -60,28 +60,27 @@ namespace Grid
             this.mapWidth = mapWidth;
             this.mapHeight = mapHeight;
             this.grassPrefabs = grassPrefabs;
-            this.waterPrefabs = waterPrefabs;
+            this.oceanPrefabs = waterPrefabs;
             this.woodresourcePrefabs = woodresourcePrefabs;
             this.diamondresourcePrefabs = diamondresourcePrefabs;
             this.obsidianresourcePrefabs = obsidianresourcePrefabs;
             this.goldresourcePrefabs = goldresourcePrefabs;
             this.stoneresourcePrefabs = stoneresourcePrefabs;
             this.vineresourcePrefabs = vineresourcePrefabs;
-            this.waterresourcePrefabs = waterresourcePrefabs;
+            this.lakePrefabs = waterresourcePrefabs;
             this.Generator = Generator;
             Tiles =  new FloorTile[mapWidth * mapHeight];
         }
 
         
 
-        public void GridGeneration(int seed)
+        public void GridGeneration(GameObject parent, int seed)
         {
-            
             Generator.Generate(this,seed);
-            GenerateHexMap(seed);
+            GenerateHexMap(parent,seed);
         }
 
-        private void GenerateHexMap(int seed)
+        private void GenerateHexMap(GameObject parent, int seed)
         {
             
             double xOffset = sqrt3*hexSize; // Décalage horizontal 
@@ -99,7 +98,8 @@ namespace Grid
                     {
                         xPos += xOffset/2;
                     }
-                    CreateHexTile(new Vector3((float)xPos, 0, (float)zPos),(this[z,x]), seed);
+
+                    CreateHexTile(parent,new Vector3((float)xPos, 0, (float)zPos),(this[z,x]), seed);
                 }
             }
         }
@@ -112,8 +112,8 @@ namespace Grid
                     return grassPrefabs;
                 case (FloorTile.Wood):
                     return woodresourcePrefabs;
-                case (FloorTile.Water):
-                    return waterresourcePrefabs;
+                case (FloorTile.Lake):
+                    return lakePrefabs;
                 case (FloorTile.DiamondMountain):
                     return diamondresourcePrefabs;
                 case (FloorTile.ObsidianMountain):
@@ -124,18 +124,20 @@ namespace Grid
                     return stoneresourcePrefabs;
                 case (FloorTile.Vine):
                     return vineresourcePrefabs;
+                case (FloorTile.Ocean):
+                    return oceanPrefabs;
                 default:
                     throw new ArgumentException("une tile qui n'existe pas essaie d'être instancié");
             }
         }
 
-        private void CreateHexTile(Vector3 position, FloorTile tile, int seed)
+        public void CreateHexTile(GameObject parent, Vector3 position, FloorTile tile, int seed)
         {
         
             GameObject hexPrefab = EnumToPrefab(tile);
 
             // Instancier le modèle sélectionné
-            GameObject hex = Instantiate(hexPrefab, this.transform);
+            GameObject hex = Object.Instantiate(hexPrefab, parent.transform);
             hex.transform.position = position;
         
 

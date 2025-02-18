a100862161fd1819958594579f1ae24496a0558c
titou4n <titou4n.simon@gmail.com>
2025-02-18 13:44:24 +0100

    restructure Menu

--- Assets/Menus/MainMenu.cs
+++ Assets/Menus/MainMenu.cs
@@ -1,19 +1,127 @@
+using System;
+using ForServer;
+using Steamworks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+namespace Menus
 {
-    public void PlayGame()
+    public class MainMenu : MonoBehaviour
     {
-        SceneManager.LoadScene("Scenes/Menus/Play/MultiplayerOrAIMenu");
-    }
-    public void OpenOptions()
-    {
-        SceneManager.LoadScene("Scenes/Menus/OptionMenu");
-    }
-    public void QuitGame()
-    {
-        Debug.Log("Quitter le jeu !");
-        Application.Quit();
+        public GameObject menuUIMainMenu;
+        public GameObject menuUIOptionsMenu;
+        public GameObject menuUIMultiplayerOrAi;
+        //public GameObject menuUIMultiplayerSteamOrTcpIp;
+        public GameObject menuUIMultiplayerHostOrJoinMenu;
+
+        private void Start()
+        {
+            SetAllMenusInactive();
+            menuUIMainMenu.SetActive(true);
+        }
+
+        private void SetAllMenusInactive()
+        {
+            menuUIMainMenu.SetActive(false);
+            menuUIOptionsMenu.SetActive(false);
+            menuUIMultiplayerOrAi.SetActive(false);
+            //menuUIMultiplayerSteamOrTcpIp.SetActive(false);
+            menuUIMultiplayerHostOrJoinMenu.SetActive(false);
+        }
+        
+        public void BackToMainMenu()
+        {
+            SetAllMenusInactive();
+            menuUIMainMenu.SetActive(true);
+        }
+
+        //__________________________________________________________//
+        //________________________MainMenu__________________________//
+        //__________________________________________________________//
+        
+        public void PlayGame()
+        {
+            SetAllMenusInactive();
+            menuUIMultiplayerOrAi.SetActive(true);
+        }
+        public void OpenOptions()
+        {
+            SetAllMenusInactive();
+            menuUIOptionsMenu.SetActive(true);
+        }
+        public void QuitGame()
+        {
+            Debug.Log("Quitter le jeu !");
+            Application.Quit();
+        }
+        
+        //__________________________________________________________//
+        //________________________OptionMenu________________________//
+        //__________________________________________________________//
+        
+        public void Option1()
+        {
+            Debug.Log("Option1");
+        }
+    
+        public void Option2()
+        {
+            Debug.Log("Option2");
+        }
+
+        public void Option3()
+        {
+            Debug.Log("Option3");
+        }
+        
+        //__________________________________________________________//
+        //________________________MultiplayerOrAi___________________//
+        //__________________________________________________________//
+
+        
+        public void PlayOnMultiplayer()
+        {
+            SetAllMenusInactive();
+            menuUIMultiplayerHostOrJoinMenu.SetActive(true);
+        }
+    
+        public void PlayWithAI() // TODO
+        {
+            SetAllMenusInactive();
+            menuUIMultiplayerHostOrJoinMenu.SetActive(true);
+        }
+        
+        //__________________________________________________________//
+        //________________________HostOrJoin________________________//
+        //__________________________________________________________//
+        
+        private void OpenFriendsOverlay()
+        {
+            if (SteamManager.Initialized)
+            {
+                SteamFriends.ActivateGameOverlay("Friends");
+                SceneManager.LoadScene("WaitingScene");
+            }
+            else
+            {
+                Debug.LogError("Steam is not initialized or not running.");
+            }
+        }
+    
+        public void Host()
+        {
+            SetAllMenusInactive();
+            CSteamID id = SteamUser.GetSteamID();
+            //SteamFriends.ActivateGameOverlayInviteDialog(id);
+        
+            ServerManager.Instance.Host();
+        
+            SceneManager.LoadScene("WaitingScene");
+        }
+    
+        public void Join()
+        {
+            OpenFriendsOverlay();
+        }
     }
 }

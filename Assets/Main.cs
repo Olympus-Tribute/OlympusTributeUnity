75c210c2780d3c60916e6250f107e21cd32f4799
T <80157600+titou4n@users.noreply.github.com>
2025-02-27 12:17:20 +0100

    ajout pour ajouter le steamAppId (#31)

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,18 @@ public class Main : MonoBehaviour
 {
     public void Start()
     {
+        CreateSteamAppIdFile();
         SceneManager.LoadScene("Scenes/MainMenu");
+    }
+
+    private static void CreateSteamAppIdFile()
+    {
+        string filePath = "steam_appid.txt";
+        
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, "480");
+            
+        }
     }
 }

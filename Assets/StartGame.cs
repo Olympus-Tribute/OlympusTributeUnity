3f538cbd9f02934d059605accd370b86b2272502
titou4n <titou4n.simon@gmail.com>
2025-02-27 19:26:31 +0100

    ajout StartGame

---
+++ Assets/StartGame.cs
@@ -0,0 +1,14 @@
+using ForNetwork;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class StartGame : MonoBehaviour
+    {
+        public void OnEnable()
+        {
+            //var action = new ClientGameStartGameAction();
+            //Network.Instance.Proxy.Connection.Send(action);
+        }
+    }
+}

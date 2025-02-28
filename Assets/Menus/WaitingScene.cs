ee7ef61199bf081c3246f685cba87a81e635f2da
Iru ^^ <irulan.sanz@epita.fr>
2025-03-01 00:55:12 +0100

    je sais pas si ça casse qqch mais bon

--- Assets/Menus/WaitingScene.cs
+++ Assets/Menus/WaitingScene.cs
@@ -11,15 +11,19 @@ public class WaitingScene : MonoBehaviour
     
     public GameObject imageReadyPlayer1;
     public GameObject imageNotReadyPlayer1;
+    public GameObject imageFlagPlayer1;
     
     public GameObject imageReadyPlayer2;
     public GameObject imageNotReadyPlayer2;
+    public GameObject imageFlagPlayer2;
     
     public GameObject imageReadyPlayer3;
     public GameObject imageNotReadyPlayer3;
+    public GameObject imageFlagPlayer3;
     
     public GameObject imageReadyPlayer4;
     public GameObject imageNotReadyPlayer4;
+    public GameObject imageFlagPlayer4;
     
     
     //___________________________________________________________//
@@ -87,15 +91,23 @@ public class WaitingScene : MonoBehaviour
 
     public void Start()
     {
-        imageReadyPlayer1.SetActive(false);
+        imageReadyPlayer1.SetActive(true);
         imageNotReadyPlayer1.SetActive(false);
+        imageFlagPlayer1.SetActive(true);
+        
         imageReadyPlayer2.SetActive(false);
         imageNotReadyPlayer2.SetActive(false);
+        imageFlagPlayer2.SetActive(false);
+        
         imageReadyPlayer3.SetActive(false);
         imageNotReadyPlayer3.SetActive(false);
+        imageFlagPlayer3.SetActive(false);
+        
         imageReadyPlayer4.SetActive(false);
         imageNotReadyPlayer4.SetActive(false);
+        imageFlagPlayer4.SetActive(false);
     }
+   
 
     public void Ready() // For Button
     {
@@ -128,6 +140,7 @@ public class WaitingScene : MonoBehaviour
                 case 0 when ready:
                     imageReadyPlayer1.SetActive(true);
                     imageNotReadyPlayer1.SetActive(false);
+                    imageFlagPlayer1.SetActive(true);
                     break;
                 case 0:
                     imageReadyPlayer1.SetActive(false);
@@ -136,6 +149,7 @@ public class WaitingScene : MonoBehaviour
                 case 1 when ready:
                     imageReadyPlayer2.SetActive(true);
                     imageNotReadyPlayer2.SetActive(false);
+                    imageFlagPlayer2.SetActive(true);
                     break;
                 case 1:
                     imageReadyPlayer2.SetActive(false);
@@ -144,6 +158,7 @@ public class WaitingScene : MonoBehaviour
                 case 2 when ready:
                     imageReadyPlayer3.SetActive(true);
                     imageNotReadyPlayer3.SetActive(false);
+                    imageFlagPlayer3.SetActive(true);
                     break;
                 case 2:
                     imageReadyPlayer3.SetActive(false);
@@ -152,6 +167,7 @@ public class WaitingScene : MonoBehaviour
                 case 3 when ready:
                     imageReadyPlayer4.SetActive(true);
                     imageNotReadyPlayer4.SetActive(false);
+                    imageFlagPlayer4.SetActive(true);
                     break;
                 case 3:
                     imageReadyPlayer4.SetActive(false);

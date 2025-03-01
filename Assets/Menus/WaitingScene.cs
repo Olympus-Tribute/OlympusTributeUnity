8bd9025ea8acb41a0aa922b5aa13d2a1d22c67d6
titou4n <titou4n.simon@gmail.com>
2025-03-01 09:53:12 +0100

    test deplace les 2 listener

--- Assets/Menus/WaitingScene.cs
+++ Assets/Menus/WaitingScene.cs
@@ -35,6 +35,13 @@ public class WaitingScene : MonoBehaviour
     {
         if (Network.Instance.Proxy is not null)
         {
+            Network.Instance.Proxy.GameActionListenerManager.AddListener<ServerReadyStatesGameAction>(
+                (connection, action) =>
+                {
+                    _readyStates = action.ReadyStates;
+                    UpdatePlayersReady();
+                });
+            
             Network.Instance.Proxy.GameActionListenerManager.AddListener<ServerStartLobbyGameAction>(
                 (connection, action) =>
                 {
@@ -46,20 +53,19 @@ public class WaitingScene : MonoBehaviour
                     ServerManager.PlayerId = action.PlayerId;
                     InitGame();
                 });
-            
-            /*
-            Network.Instance.Proxy.GameActionListenerManager.AddListener<ServerReadyStatesGameAction>(
-                (connection, action) =>
-                {
-                    _readyStates = action.ReadyStates;
-                    UpdatePlayersReady();
-                });
-            */
         }
         else
         {
             Network.Instance.Setup = (proxy =>
             {
+                proxy.GameActionListenerManager.AddListener<ServerReadyStatesGameAction>(
+                    (connection, action) =>
+                    {
+                        _readyStates = action.ReadyStates;
+                        UpdatePlayersReady();
+                    });
+                
+                
                 proxy.GameActionListenerManager.AddListener<ServerStartLobbyGameAction>(
                     (connection, action) =>
                     {
@@ -71,27 +77,8 @@ public class WaitingScene : MonoBehaviour
                         ServerManager.PlayerId = action.PlayerId;
                         InitGame();
                     });
-                /*
-                proxy.GameActionListenerManager.AddListener<ServerReadyStatesGameAction>(
-                    (connection, action) =>
-                    {
-                        _readyStates = action.ReadyStates;
-                        UpdatePlayersReady();
-                    });
-                */
             });
         }
-
-        if (Network.Instance.Proxy != null)
-        {
-            Network.Instance.Proxy.GameActionListenerManager.AddListener<ServerReadyStatesGameAction>(
-                (connection, action) =>
-                {
-                    _readyStates = action.ReadyStates;
-                    UpdatePlayersReady();
-                });
-        }
-            
     }
     
     //___________________________________________________________//
@@ -101,7 +88,7 @@ public class WaitingScene : MonoBehaviour
     public void Start()
     {
         SetAllActiveFalse();
-        _readyStates = new[] { true };
+        _readyStates = new[] { false };
         UpdatePlayersReady();
     }
 
@@ -202,7 +189,7 @@ public class WaitingScene : MonoBehaviour
                 case 3:
                     imageReadyPlayer4.SetActive(false);
                     imageNotReadyPlayer4.SetActive(true);
-                    imageFlagPlayer3.SetActive(true);
+                    imageFlagPlayer4.SetActive(true);
                     break;
             }
         }

71ba555381e6f10519f69a64d3b744c7c60c54b8
zhuzh <zhufanny2006@gmail.com>
2025-05-20 22:18:53 +0200

    change heightOffset from public to private

--- Assets/AnimationAttack/AnimZeusHori.cs
+++ Assets/AnimationAttack/AnimZeusHori.cs
@@ -5,10 +5,10 @@ public class Anim_ZeusHorizontal : MonoBehaviour
 {
     public GameObject lightningPrefab;          
     public PlayableDirector lightningTimeline; 
-    public float heightOffset = 1f;            
+    private float heightOffset = 1f;            
 
     [HideInInspector]
-    public Vector3 targetPosition; // fournie dynamiquement à l’instanciation
+    public Vector3 targetPosition; // fournie à l’instanciation
 
     void Start()
     {
@@ -25,12 +25,10 @@ public class Anim_ZeusHorizontal : MonoBehaviour
 
         Vector3 spawnPosition = transform.position + Vector3.up * heightOffset;
 
-        // Calcul de la direction vers la cible
         Vector3 direction = targetPosition - spawnPosition;
         float angleY = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.Euler(0f, angleY, 0f);
 
-        // Instanciation orientée de la foudre
         GameObject lightning = Instantiate(lightningPrefab, spawnPosition, rotation);
 
         if (lightningTimeline != null)

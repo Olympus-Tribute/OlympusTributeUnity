bed3cc380bf3255545f50078fcb7b0b26190d151
zhuzh <zhufanny2006@gmail.com>
2025-05-20 22:17:56 +0200

    fix delete spawn

--- Assets/Animation/Attacks/AnimAthena.cs
+++ Assets/Animation/Attacks/AnimAthena.cs
@@ -1,70 +1,77 @@
+using System.Collections;
 using UnityEngine;
 
-public class RevolutionHammer : MonoBehaviour
+namespace Animation.Attacks
 {
-    public GameObject spearPrefab;  
-    private float spawnHeight = -5f; 
-    private float targetHeight = 9f; 
-    private float moveSpeed = 5f; 
-    private float spawnInterval = 0.15f; 
-    private float attackDuration = 6f; 
 
-    private float timer = 0f;
-    private float attackTimer = 0f;
-    private bool onAttack = true; 
-    private float hexagonRadius = 10f; 
-
-    void Update()
+    public class AnimAthena : MonoBehaviour
     {
-        if (!onAttack) return; 
+        public GameObject spearPrefab;
+        private const float spawnHeight = -5f;
+        private const float targetHeight = 9f;
+        private const float moveSpeed = 5f;
+        private const float spawnInterval = 0.15f;
+        private const float attackDuration = 6f;
 
-        timer += Time.deltaTime;
-        attackTimer += Time.deltaTime;
+        private float timer = 0f;
+        private float attackTimer = 0f;
+        private bool onAttack = true;
+        private const float hexagonRadius = 10f;
 
-        if (attackTimer >= attackDuration)
+        void Update()
         {
-            onAttack = false; 
-            Destroy(this.gameObject);
-            Debug.Log("Pluie de lances terminée !");
-            return;
+            if (!onAttack) return;
+
+            timer += Time.deltaTime;
+            attackTimer += Time.deltaTime;
+            
+            
+            if (attackTimer >= attackDuration + 5f)
+            {
+                onAttack = false;
+                Destroy(gameObject);
+                return;
+            }
+
+            if (timer >= spawnInterval && attackTimer< attackDuration)
+            {
+                SpawnSpear();
+                timer = 0f;
+            }
         }
-        if (timer >= spawnInterval)
+
+        void SpawnSpear()
         {
-            SpawnSpear();
-            timer = 0f;
+            Vector3 randomPosition = GetRandomPositionInsideHexagon();
+            randomPosition.y = spawnHeight;
+
+            GameObject spear = Instantiate(spearPrefab, randomPosition, Quaternion.identity);
+
+            StartCoroutine(MoveSpear(spear));
         }
-    }
 
-    void SpawnSpear()
-    {
-        Vector3 randomPosition = GetRandomPositionInsideHexagon();  
-        randomPosition.y = spawnHeight; 
-
-        GameObject spear = Instantiate(spearPrefab, randomPosition, Quaternion.identity); 
-
-        StartCoroutine(MoveSpear(spear)); 
-    }
-
-    Vector3 GetRandomPositionInsideHexagon()
-    {
-        float angle = Random.Range(0f, 360f);
-        float randomRadius = Random.Range(0f, hexagonRadius); 
-
-        float x = randomRadius * Mathf.Cos(angle * Mathf.Deg2Rad);
-        float z = randomRadius * Mathf.Sin(angle * Mathf.Deg2Rad);
-
-        Vector3 hexagonCenter = this.transform.position;
-        return new Vector3(hexagonCenter.x + x, hexagonCenter.y, hexagonCenter.z + z);
-    }
-
-
-    System.Collections.IEnumerator MoveSpear(GameObject spear)
-    {
-        while (spear.transform.position.y < targetHeight)
+        Vector3 GetRandomPositionInsideHexagon()
         {
-            spear.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
-            yield return null; 
+            float angle = Random.Range(0f, 360f);
+            float randomRadius = Random.Range(0f, hexagonRadius);
+
+            float x = randomRadius * Mathf.Cos(angle * Mathf.Deg2Rad);
+            float z = randomRadius * Mathf.Sin(angle * Mathf.Deg2Rad);
+
+            Vector3 hexagonCenter = this.transform.position;
+            return new Vector3(hexagonCenter.x + x, hexagonCenter.y, hexagonCenter.z + z);
         }
-        Destroy(spear);
+
+
+        private IEnumerator MoveSpear(GameObject spear)
+        {
+            while (spear.transform.position.y < targetHeight)
+            {
+                spear.transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+                yield return null;
+            }
+
+            Destroy(spear);
+        }
     }
 }
